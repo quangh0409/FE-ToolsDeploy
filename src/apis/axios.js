46eefcf6e8a02cccb846d2e68423c1104e2b12f5
quanh0409 <trongquangvu80@gmail.com>
2024-06-01 22:57:40 +0700

    Fix: call be htpps

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const host = process.env.REACT_APP_CA_BE_FE_HOST
-const port = process.env.REACT_APP_CA_BE_FE_PORT
+const host = process.env.REACT_APP_CA_BE_FE_HOST;
+const port = process.env.REACT_APP_CA_BE_FE_PORT;
 const axiosServer = axios.create({
   // baseURL: "http:///api/v1/",
-  baseURL: `http://${host}:${port}/api/v1/`,
+  baseURL: `https://${host}:${port}/api/v1/`,
 });
 axiosServer.interceptors.request.use(async (config) => {
   const accessToken = localStorage.getItem("accessToken");

0a3616101f2c0528326a17bca65d789e864e2cd8
quanh0409 <trongquangvu80@gmail.com>
2024-06-04 13:32:18 +0700

    Fix: connect https

--- src/utils/socket/socket.js
+++ src/utils/socket/socket.js
@@ -3,7 +3,7 @@ const host = process.env.REACT_APP_CA_BE_FE_HOST;
 const port = process.env.REACT_APP_CA_BE_FE_PORT;
 const socket = io(`https://${host}:8080/`, {
   autoConnect: true,
-  transports: ["websocket"], // Sử dụng WebSocket để giảm thiểu mất kết nối
+  // transports: ["websocket"], // Sử dụng WebSocket để giảm thiểu mất kết nối
   reconnection: true, // Tự động kết nối lại
   reconnectionDelay: 1000, // Thời gian chờ giữa các lần kết nối lại (ms)
   reconnectionAttempts: 3,

8b4aef4af9ebe49646ab486d7776f860cdefca97
quanh0409 <trongquangvu80@gmail.com>
2024-06-04 13:28:14 +0700

    Fix: connect https

--- src/utils/socket/socket.js
+++ src/utils/socket/socket.js
@@ -1,12 +1,13 @@
 import { io } from "socket.io-client";
-const host = process.env.REACT_APP_CA_BE_FE_HOST
-const port = process.env.REACT_APP_CA_BE_FE_PORT
+const host = process.env.REACT_APP_CA_BE_FE_HOST;
+const port = process.env.REACT_APP_CA_BE_FE_PORT;
 const socket = io(`https://${host}:8080/`, {
   autoConnect: true,
-  transports: ['websocket'], // Sử dụng WebSocket để giảm thiểu mất kết nối
-  reconnection: true,         // Tự động kết nối lại
-  reconnectionDelay: 1000,   // Thời gian chờ giữa các lần kết nối lại (ms)
-  reconnectionAttempts: 3,    // Số lần thử kết nối lại tối đa
+  transports: ["websocket"], // Sử dụng WebSocket để giảm thiểu mất kết nối
+  reconnection: true, // Tự động kết nối lại
+  reconnectionDelay: 1000, // Thời gian chờ giữa các lần kết nối lại (ms)
+  reconnectionAttempts: 3,
+  secure: true, // Số lần thử kết nối lại tối đa
 });
 
 // const socket = io(`http://103.166.185.48:8080`, {
